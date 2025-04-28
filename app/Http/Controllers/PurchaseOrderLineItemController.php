2f19f6cbc869c11ba4bfb414bf02fa375330eae5
DevinNorgarb <devin.norgarb@pargo.co.za>
2025-04-28 09:53:18 +0200

    Resource Controllers + Routes

---
+++ app/Http/Controllers/PurchaseOrderLineItemController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseOrderLineItem;
+use Illuminate\Http\Request;
+
+class PurchaseOrderLineItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PurchaseOrderLineItem $purchaseOrderLineItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PurchaseOrderLineItem $purchaseOrderLineItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PurchaseOrderLineItem $purchaseOrderLineItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PurchaseOrderLineItem $purchaseOrderLineItem)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\DistributorController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseOrderController;
+use App\Http\Controllers\PurchaseOrderLineItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('suppliers', SupplierController::class);
+Route::resource('distributors', DistributorController::class);
+Route::resource('products', ProductController::class);
+Route::resource('purchase-orders', PurchaseOrderController::class);
+Route::resource('purchase-order-line-items', PurchaseOrderLineItemController::class);
